fixing softmax gradients

cs682/assignment1/cs682/classifiers/linear_svm.py
@@ -114,7 +114,7 @@ def svm_loss_vectorized(W, X, y, reg):
   masksum=np.sum(mask,axis=1)
   defs=-masksum[...,np.newaxis]*X
   np.add.at(dW.T,y,defs) #this allows repeated index addition.
-
+  
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################

cs682/assignment1/cs682/classifiers/neural_net.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import numpy as np
 import matplotlib.pyplot as plt
+from cs682.classifiers.softmax import *
 
 class TwoLayerNet(object):
   """
@@ -82,7 +83,7 @@ class TwoLayerNet(object):
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
-    
+
     # If the targets are not given then jump out, we're done
     if y is None:
       return scores
@@ -95,7 +96,9 @@ class TwoLayerNet(object):
     # in the variable loss, which should be a scalar. Use the Softmax           #
     # classifier loss.                                                          #
     #############################################################################
-    l1,g1 = softmax_loss_vectorized(X,y,W1)
+    l2reg_w1 = reg*(np.sum(W1*W1))
+    loss,grad = softmax_loss_vectorized(W2,r1_relu,y,reg,bias=b2)
+    loss += l2reg_w1
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -107,6 +110,11 @@ class TwoLayerNet(object):
     # and biases. Store the results in the grads dictionary. For example,       #
     # grads['W1'] should store the gradient on W1, and be a matrix of same size #
     #############################################################################
+    pscores_exp_rowwise_sum = np.sum(pscores_exp, axis=1)
+    #dfdq = r1_relu - W2
+    dqdw1 = None
+    dqdb1 = None
+
     pass
     #############################################################################
     #                              END OF YOUR CODE                             #

cs682/assignment1/cs682/classifiers/softmax.py
@@ -36,10 +36,12 @@ def softmax_loss_naive(W, X, y, reg):
     correct_class_exp_score = exp(scores[y[i]])
     sum_exp_scores = 0
     for sc in scores:
-        sum_exp_scores += exp(sc)
+      sum_exp_scores += exp(sc)
     loss += -log(correct_class_exp_score/sum_exp_scores)
     for j in range(num_classes):
-      dW[:,j] += X[i]*((exp(scores[j])/sum_exp_scores) - correct_class_exp_score)
+      dW[:,j] += X[i]*(exp(scores[j])/sum_exp_scores)
+      if j==y[i]:
+        dW[:,j] -= X[i]
   loss /= num_train
   dW /= num_train
   loss += reg * np.sum(W * W)
@@ -50,7 +52,7 @@ def softmax_loss_naive(W, X, y, reg):
   return loss, dW
 
 
-def softmax_loss_vectorized(W, X, y, reg):
+def softmax_loss_vectorized(W, X, y, reg,bias=None):
   """
   Softmax loss function, vectorized version.
 
@@ -67,6 +69,8 @@ def softmax_loss_vectorized(W, X, y, reg):
   #############################################################################
   num_train = X.shape[0] # N
   pscores = X.dot(W) # N x C
+  if bias is not None:
+    pscores+=bias
   pscores_exp = np.exp(pscores) # N x C
   correct_class_pscores_idx = (np.arange(0,num_train,1),(y))
   correct_class_pscores = pscores_exp[correct_class_pscores_idx] # N
@@ -79,9 +83,8 @@ def softmax_loss_vectorized(W, X, y, reg):
 
   pscores_exp_rowwise_sum = pscores_exp_rowwise_sum.reshape(num_train,1) # N
   pscores_exp_norma = pscores_exp/pscores_exp_rowwise_sum # N x C
-  pscores_exp_norma_minus_correct = pscores_exp_norma - correct_class_pscores.reshape(num_train,1)
-
-  dW = X.T.dot(pscores_exp_norma_minus_correct)
+  dW = X.T.dot(pscores_exp_norma) #until this point we only have dL/dWj
+  np.add.at(dW.T,y,-X) #taking care of the dL/dWyi
   dW/=num_train
   #############################################################################
   #                          END OF YOUR CODE                                 #