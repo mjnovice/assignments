0e3d10ea9d8cd6d1409dfe535d86168f3b9512cf
Mayank Jha <mayank25080562@gmail.com>
2021-02-18 08:52:41 -0500

    adding cache to pytorch

---
+++ cs682/assignment1/cifar.py
@@ -0,0 +1,253 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from collections import defaultdict, deque
+import itertools
+########################################################################
+# The output of torchvision datasets are PILImage images of range [0, 1].
+# We transform them to Tensors of normalized range [-1, 1].
+# .. note::
+#     If running on Windows and you get a BrokenPipeError, try setting
+#     the num_worker of torch.utils.data.DataLoader() to 0.
+
+
+def trans_id(op):
+    if hasattr(op,'__name__'):
+        return op.__name__
+    return repr(op)
+
+def getKey(ops,objName):
+    key=""
+    for op in ops:
+        key+=trans_id(op)+"_"
+    key+=objName
+    return key
+
+cache = "cache"
+
+pipeline = [
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+
+import os
+from pathlib import Path
+
+def transformsWithId(image_id,image):
+    global pipeline,cache
+    ops=pipeline
+    for i,op in enumerate(ops):
+        key = "/tmp/tensors/" +getKey(ops[:i+1],str(image_id))
+        if cache!=None:
+            if Path(key).is_file():
+                image=torch.load(key)
+                #print("cache hit")
+                continue
+        image=op(image)
+        if cache!=None:
+            torch.save(image,key)
+    return image
+
+
+
+
+class Cifar5000(torchvision.datasets.CIFAR10):
+    def __init__(self, root=None, transforms=None, train=True):
+        super().__init__(root, train, download=True)
+        self.transforms = transforms
+        self.n_images_per_class = 1000
+        self.n_classes = 10
+        self.new2old_indices = self.create_idx_mapping()
+
+    def create_idx_mapping(self):
+        label2idx = defaultdict(lambda: deque(maxlen=self.n_images_per_class))
+        for original_idx in range(super().__len__()):
+            _, label = super().__getitem__(original_idx)
+            label2idx[label].append(original_idx)
+
+        old_idxs = set(itertools.chain(*label2idx.values()))
+        new2old_indices = {}
+        for new_idx, old_idx in enumerate(old_idxs):
+            new2old_indices[new_idx] = old_idx
+
+        return new2old_indices
+
+    def __len__(self):
+        return len(self.new2old_indices)
+
+    def __getitem__(self, index):
+        index = self.new2old_indices[index]
+        im, label = super().__getitem__(index)
+        return self.transforms(index,im), label
+
+
+trainset = Cifar5000(root='./data', transforms=transformsWithId, train=True)
+print(len(trainset))
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transforms.Compose(pipeline))
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+########################################################################
+# 2. Define a Convolutional Neural Network
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Copy the neural network from the Neural Networks section before and modify it to
+# take 3-channel images (instead of 1-channel images as it was defined).
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net1 = Net()
+net2 = Net()
+
+########################################################################
+# 3. Define a Loss function and optimizer
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Let's use a Classification Cross-Entropy loss and SGD with momentum.
+
+import torch.optim as optim
+
+def train(trainloader,net):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    ########################################################################
+    # 4. Train the network
+    # ^^^^^^^^^^^^^^^^^^^^
+    #
+    # This is when things start to get interesting.
+    # We simply have to loop over our data iterator, and feed the inputs to the
+    # network and optimize.
+
+    for epoch in range(1):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+    print('Finished Training ')
+import time
+s0 = time.time()
+print("Training with no cache")
+train(trainloader,Net())
+s1=time.time()
+print("Training with empty cache!")
+train(trainloader,net1)
+s2=time.time()
+print("Training with warmed cache")
+train(trainloader,net2)
+s3=time.time()
+print("NoCache: ",s1-s0)
+print("EmptyCache: ",s2-s1)
+print("WarmedCache: ",s3-s2)
+print("percentage gain: ",(2*s2-s3-s1)*100/(s2-s1))
+########################################################################
+# Let's quickly save our trained model:
+
+PATH = './cifar_net.pth'
+torch.save(net1.state_dict(), PATH)
+
+########################################################################
+# See `here <https://pytorch.org/docs/stable/notes/serialization.html>`_
+# for more details on saving PyTorch models.
+#
+# 5. Test the network on the test data
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# We have trained the network for 2 passes over the training dataset.
+# But we need to check if the network has learnt anything at all.
+#
+# We will check this by predicting the class label that the neural network
+# outputs, and checking it against the ground-truth. If the prediction is
+# correct, we add the sample to the list of correct predictions.
+#
+# Okay, first step. Let us display an image from the test set to get familiar.
+
+net = Net()
+net.load_state_dict(torch.load(PATH))
+
+########################################################################
+# The results seem pretty good.
+#
+# Let us look at how the network performs on the whole dataset.
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+########################################################################
+# That looks way better than chance, which is 10% accuracy (randomly picking
+# a class out of 10 classes).
+# Seems like the network learnt something.
+#
+# Hmmm, what are the classes that performed well, and the classes that did
+# not perform well:
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
